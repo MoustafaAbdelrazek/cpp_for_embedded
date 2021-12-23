45a1f774c8fa39b4b67a105efb3df97fa49c2402
MSI\mostafa <Moustafahmadar@gmail.com>
2021-12-23 15:44:35 +0200

    This is my first cpp project

---
+++ lab1_helloworld/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "print.h"
+using namespace std;
+
+int main(void){
+    print();
+    return 0;
+}
---
+++ lab1_helloworld/print.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+void print(){
+    cout<<"I'm Moustafa Ahmed Abdelrazek\n";
+    
+}
